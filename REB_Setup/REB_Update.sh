55a2d8c3543d465179f3ef29e543a08dee203713
Rich Colvin <44912249+RichColvin@users.noreply.github.com>
2026-01-22 15:26:23 -0500

    Update for Trixie

--- REB_Setup/REB_Update.sh
+++ REB_Setup/REB_Update.sh
@@ -1,4 +1,4 @@
-#! /home/cnc/linuxcnc/
+#! /home/reuben/linuxcnc/
 #######################################################################
 #                    RRRRRR    EEEEEEEE  BBBBBBB                      #
 #                    RR   RR   EE        BB    BB                     #
@@ -30,35 +30,40 @@
 #   1.0 - 27 Oct 2025, R. Colvin
 #   1.1 - 23 Dec 2025, R. Colvin - Changed text from "upgrade" to
 #         "update".
+#   1.2 - 22 January 2026, R. Colvin - Updated to run on Debian 13
+#             Trixie: updated directories from
+#                /home/reuben/
+#             to
+#                /home/reuben/
 #
-# Copyright (c) 2025 Colvin Tools and Brainwave Embedded. 
+# Copyright (c) 2026 Colvin Tools and Brainwave Embedded.
 #
 # The following MIT/X Consortium License applies to the Rose Engine
-# Butler system.  Use of this system constitutes consent to the terms 
+# Butler system.  Use of this system constitutes consent to the terms
 # outlined below.
 #
-# Permission is hereby granted, free of charge, to any person obtaining 
-# a copy of this software and associated documentation files (the 
-# "Software"), to deal in the Software without restriction, including 
-# without limitation the rights to use, copy, modify, merge, publish, 
-# distribute, sublicense, and/or sell copies of the Software, and to 
-# permit persons to whom the Software is furnished to do so, subject to 
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
 # the following conditions:
 #
-# The above copyright notice and this permission notice shall be 
+# The above copyright notice and this permission notice shall be
 # included in all copies or substantial portions of the Software.
 #
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
-# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
 # MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY 
-# CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, 
-# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+# CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 # SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #
 # Except as contained in this notice, the name of COPYRIGHT HOLDERS
-# shall not be used in advertising or otherwise to promote the sale, 
-# use or other dealings in this Software without prior written 
+# shall not be used in advertising or otherwise to promote the sale,
+# use or other dealings in this Software without prior written
 # authorization from COPYRIGHT HOLDERS.
 #
 # ********************************************************************
@@ -88,7 +93,7 @@ echo -e "${TITLE}Update the system                                              
 echo -e "${TITLE}                                                                       ${NOCOLOR}"
 echo -e "${TITLE}#######################################################################${NOCOLOR}"
 echo -e "${TITLE}Pull latest files from GitHub                                          ${NOCOLOR}"
-cd /home/cnc/linuxcnc/configs/RoseEngineButler
+cd /home/reuben/linuxcnc/configs/RoseEngineButler
 sudo git stash
 sudo git pull
 if [ $? != 0 ]; then
